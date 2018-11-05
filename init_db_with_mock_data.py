8ba077e5933028ce0d2c8824b7b653682f6cfea7
Bjarni Jens Kristinsson <bjarni.jens@gmail.com>
2018-11-05 21:03:40 +0000

    Description for the first three cars now >100 characters

--- init_db_with_mock_data.py
+++ init_db_with_mock_data.py
@@ -38,7 +38,8 @@ ad_ferrari_f50, created = Advertisement.objects.get_or_create(
     car=ferrari_f50,
     image_url="https://www.supercars.net/blog/wp-content/uploads/2016/05/ferrari_f50_preserial_red_front_view_100209_1920x1080.jpg",
     price=15,
-    currency='BTC'
+    currency='BTC',
+    description="Car for winners. Be a winner. Have a Ferrari F50. Be admired. Be an alpha. Be cool. Be chill. Drive safely."
 )
 
 # Toyota Corolla
@@ -57,7 +58,7 @@ ad_toyota_corolla, created = Advertisement.objects.get_or_create(
     image_url="https://upload.wikimedia.org/wikipedia/commons/d/dd/2018_Toyota_Corolla_%28ZRE172R%29_Ascent_sedan_%282018-08-27%29.jpg",
     price=3990000,
     currency='ISK',
-    description="100% NEW -- has never been used!"
+    description="100% NEW -- has never been used! A really, really good car. It uses very little gasoline and doesn't make much sound."
 )
 
 # Toyota Hilux
@@ -75,5 +76,6 @@ ad_toyota_hilux, created = Advertisement.objects.get_or_create(
     car=toyota_hilux,
     image_url="http://wallpapersqq.net/wp-content/uploads/2016/04/toyota-hilux-2016-blue-new-2016-picture.jpg",
     price=1590000,
-    currency='ISK'
+    currency='ISK',
+    description="Chrashed it once when doing coke with my stripper girlfriend but the cop came and was very understanding so I slept it off at the station (girlfriend left me that night) and paid a small fine the day after. Me and the cop are best friends now."
 )
