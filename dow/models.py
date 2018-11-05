43da9bc64a494122cfbee9696b10532f6393f087
Bjarni Jens Kristinsson <bjarni.jens@gmail.com>
2018-11-05 20:36:47 +0000

    Recursively returning info on ForeignKey objects in JSON API endpoints instead of the key id itself

--- dow/models.py
+++ dow/models.py
@@ -2,12 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# Overwriting 'User' class 'natural_key' function to better fit our format
+def user_natural_key(self):
+    return {'username': self.get_username()}
+
+
+User.natural_key = user_natural_key
+
+
 class Manufacturer(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=1000, blank=True)
 
     def get_list_of_manufacturers(self):
         return [str(m) for m in self.Manufacturer.all()]
+
+    def natural_key(self):
+        return {'name': self.name, 'description': self.description}
 
     def __str__(self):
         return self.name
@@ -25,12 +36,25 @@ class Car(models.Model):
     next_check = models.DateField(null=True, blank=True)
     pollution = models.CharField(max_length=100, null=True, blank=True)
 
-
     def get_list_of_cars(self):
         return [str(c) for c in self.Car.all()]
 
+    def natural_key(self):
+        return {
+            'manufacturer': self.manufacturer.name,
+            'model': self.model,
+            'registration_number': self.registration_number,
+            'color': self.color,
+            'driven': self.driven,
+            'year': self.year,
+            'weight': self.weight,
+            'registered_at': self.registered_at,
+            'next_check': self.next_check,
+            'pollution': self.pollution
+        }
+
     def __str__(self):
-        return "{} {}, {}".format(self.manufacturer, self.model, self.year)
+        return "({}) {} {}, {}".format(self.registration_number, self.manufacturer, self.model, self.year)
 
 
 class Advertisement(models.Model):
--- dow/views.py
+++ dow/views.py
@@ -9,17 +9,17 @@ def index(request):
 
 def all_manufacturers_json(request):
     all_manufacturers = Manufacturer.objects.all()
-    data = serializers.serialize('json', all_manufacturers)
+    data = serializers.serialize('json', all_manufacturers, use_natural_foreign_keys=True, use_natural_primary_keys=True)
     return HttpResponse(data, content_type="application/json")
 
 
 def all_cars_json(request):
     all_cars = Car.objects.all()
-    data = serializers.serialize('json', all_cars)
+    data = serializers.serialize('json', all_cars, use_natural_foreign_keys=True, use_natural_primary_keys=True)
     return HttpResponse(data, content_type="application/json")
 
 
 def all_advertisements_json(request):
     all_advertisements = Advertisement.objects.all()
-    data = serializers.serialize('json', all_advertisements)
+    data = serializers.serialize('json', all_advertisements, use_natural_foreign_keys=True, use_natural_primary_keys=True)
     return HttpResponse(data, content_type="application/json")
