cf61224d8184de54a852ddc4ba2ee488ac9a29ab
Bjarni Jens Kristinsson <bjarni.jens@gmail.com>
2018-11-06 00:44:04 +0000

    Expecting and handling QueryDict now as requst.POST when creating a new ad

--- dow/views.py
+++ dow/views.py
@@ -11,7 +11,8 @@ def index(request):
 
 def all_manufacturers_json(request):
     all_manufacturers = Manufacturer.objects.all()
-    data = serializers.serialize('json', all_manufacturers, use_natural_foreign_keys=True, use_natural_primary_keys=True)
+    data = serializers.serialize('json', all_manufacturers, use_natural_foreign_keys=True,
+                                 use_natural_primary_keys=True)
     return HttpResponse(data, content_type="application/json")
 
 
@@ -23,7 +24,8 @@ def all_cars_json(request):
 
 def all_advertisements_json(request):
     all_advertisements = Advertisement.objects.all()
-    data = serializers.serialize('json', all_advertisements, use_natural_foreign_keys=True, use_natural_primary_keys=True)
+    data = serializers.serialize('json', all_advertisements, use_natural_foreign_keys=True,
+                                 use_natural_primary_keys=True)
     return HttpResponse(data, content_type="application/json")
 
 
@@ -32,37 +34,41 @@ def create_advertisement(request):
         # 405 Method Not Allowed
         return HttpResponse(status=405)
 
-    json_object = json.loads(request.POST)
+    required_attributes = ['username', 'manufacturer', 'model', 'registration_number', 'color', 'driven', 'year',
+                           'image_url', 'price']
+    for attr in required_attributes:
+        if attr not in request.POST:
+            # 400 Bad Request
+            return HttpResponse(status=400)
 
     user, created = User.objects.get_or_create(
-        username = json_object.get('username', 'SecretUser'),
-        password = 'password',
-        email = '{}@example.org'.format(json_object.get('username', 'SecretUser'))
+        username=request.POST.get('username'),
+        password='password',
+        email='{}@example.org'.format(request.POST.get('username'))
     )
     manufacturer, created = Manufacturer.objects.get_or_create(
-        name = json_object.get('manufacturer', None),
-        description = ""
+        name=request.POST.get('manufacturer'),
+        description=""
     )
     car, created = Car.objects.get_or_create(
-        manufacturer = manufacturer,
-        model = json_object.get('model', None),
-        registration_number = json_object.get('registration_number', None),
-        color = json_object.get('color', None),
-        driven = json_object.get('driven', None),
-        year = json_object.get('year', None),
-        weight = json_object.get('weight', None),
-        registered_at = json_object.get('registered_at', None),
-        next_check = json_object.get('next_check', None),
-        pollution = json_object.get('pollution', None)
+        manufacturer=manufacturer,
+        model=request.POST.get('model'),
+        registration_number=request.POST.get('registration_number'),
+        color=request.POST.get('color'),
+        driven=request.POST.get('driven'),
+        year=request.POST.get('year'),
+        weight=request.POST.get('weight', None),
+        registered_at=request.POST.get('registered_at', None),
+        next_check=request.POST.get('next_check', None),
+        pollution=request.POST.get('pollution', None)
     )
     advertisement, created = Advertisement.objects.get_or_create(
-        seller = user,
-        car = car,
-        image_url = json_object.get('image_url', None),
-        price = json_object.get('price', None),
-        description = json_object.get('description', None)
+        seller=user,
+        car=car,
+        image_url=request.POST.get('image_url'),
+        price=request.POST.get('price'),
+        description=request.POST.get('description', None)
     )
 
-    # Return a "created" (201) response code.
+    # 201 Created
     return HttpResponse(status=201)
-
